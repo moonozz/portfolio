8beb6bcbb1d19604db3a1e30ab0d1e58d701c192
moonozz <http0127@naver.com>
2023-09-19 21:35:47 +0900

    :zap: Feat: Card 컴포넌트생성

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import styled, { withTheme } from 'styled-components';
 import Tag from './Tag';
-import perpettMain from '../images/perpett-main.png';
 
-function Card({ text, items }) {
+function Card({ id, text, items, imgUrl, theme }) {
   return (
-    <CardDiv>
-      <Img src={perpettMain} alt="퍼펫트" />
-      <Txt>
-        <H3 className="clashFont">{text}</H3>
-        <Tag items={items} />
-      </Txt>
-    </CardDiv>
+    <Link to={`/p/${id}`}>
+      <CardDiv>
+        <LinkDiv>
+          {/* <Img src={imgUrl} alt="퍼펫트" /> */}
+          <Img imgUrl={imgUrl} />
+          <Content>
+            <Title>{text}</Title>
+            <TagGroup>
+              <Tag items={items} borderColor={theme.color.mainDark} />
+            </TagGroup>
+          </Content>
+        </LinkDiv>
+      </CardDiv>
+    </Link>
   );
 }
 
-export default Card;
+export default withTheme(Card);
 
 const CardDiv = styled.div`
   display: flex;
@@ -23,18 +30,63 @@ const CardDiv = styled.div`
   margin-bottom: 2.4rem;
 `;
 
-const Img = styled.img`
+const LinkDiv = styled.a`
+  /* display: flex;
+  flex-direction: column; */
+  &:hover {
+    text-decoration: none;
+    font-weight: 900;
+    /* transition: width 2s linear 4s; */
+    /* transition: all 3s ease-in-out 8s; */
+    /* transition: all cubic-bezier(0.4, 0, 1, 1) 300ms; */
+  }
+`;
+
+const Img = styled.div`
+  position: relative;
   border-radius: 1.2rem;
-  width: 50px;
-  height: 50px;
+  width: 100%;
+  height: 12rem;
+  background-image: url(${({ imgUrl }) => imgUrl});
+  background-repeat: no-repeat;
+  background-size: cover;
+  border: 0.1rem solid ${({ theme }) => theme.color.mainDark};
+  /* display: flex; */
+  /* justify-content: center; */
+  /* align-items: center; */
+  &:hover {
+    transition: all cubic-bezier(0.4, 0, 1, 1) 300ms;
+    height: 30.8rem;
+  }
+  &::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    content: '';
+    background-color: ${({ theme }) => theme.color.mainDark};
+    /* mix-blend-mode: normal; */
+    opacity: 0.7;
+    border-radius: 1.2rem;
+  }
 `;
 
-const Txt = styled.div`
+const Content = styled.div`
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
+  align-items: center;
 `;
 
-const H3 = styled.h3`
+const TagGroup = styled.div`
+  display: flex;
+`;
+
+const Title = styled.h4`
   color: ${({ theme }) => theme.color.mainDark};
+  font-family: 'Clash Display';
+  font-size: 2.4rem;
+  &:hover {
+    /* font-weight: 900; */
+  }
 `;
