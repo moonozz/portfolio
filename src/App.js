79875b7c50679182b0d59262c367650054f2a9e7
moonozz <http0127@naver.com>
2023-09-23 14:30:25 +0900

    :zap: Feat: 프로덕트 페이지 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Home from './pages/Home';
-import Perpett from './pages/Dev/Perpett';
-import Poopoo from './pages/Dev/Poopoo';
-import DarkTODO from './pages/Dev/DarkTodo';
-import NomadTODO from './pages/Dev/NomadTodo';
-import Geeks from './pages/Des/Geeks';
-import Yellee from './pages/Des/Yellee';
-import Idus from './pages/Des/Idus';
-import Dokrip from './pages/Des/Dokripwoondong';
+import ProductPage from './pages/ProductPage';
 import Header from './component/Header';
 import Footer from './component/Footer';
 
@@ -21,14 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/p/1" element={<Perpett />} />
-        <Route path="/p/2" element={<Poopoo />} />
-        <Route path="/p/3" element={<DarkTODO />} />
-        <Route path="/p/4" element={<NomadTODO />} />
-        <Route path="/p/5" element={<Geeks />} />
-        <Route path="/p/6" element={<Yellee />} />
-        <Route path="/p/7" element={<Idus />} />
-        <Route path="/p/8" element={<Dokrip />} />
+        <Route path="/p/:id" element={<ProductPage />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { ReactComponent as BtnFigma } from '../images/figma.svg';
+import { ReactComponent as BtnGithub } from '../images/github.svg';
+import { setProject, setBack } from '../actions/actions';
+
+import Tag from '../component/Tag';
+
+function ProductPage() {
+  const dispatch = useDispatch();
+  const project = useSelector(state => state.project);
+
+  const getLocalBack = localStorage.getItem('back');
+  const getLocalProject = localStorage.getItem('project');
+
+  const data = {};
+
+  const localSetProject = () => {
+    localStorage.setItem('project', JSON.stringify(project));
+  };
+
+  const parsedData = JSON.parse(getLocalProject);
+  localSetProject();
+
+  useEffect(() => {
+    window.scroll(0, 0);
+
+    dispatch(setBack(getLocalBack));
+    console.log(project);
+    // dispatch(setProject(parsedData));
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <h2 className="title">{project.title}</h2>
+        <MainImg className="main-img" />
+        <div className="project-info">
+          <div className="project-title-group">
+            <div className="project-info-title">
+              <h3>{project.title}</h3>
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link"
+              >
+                Link
+                <span />
+              </a>
+            </div>
+            <ProjectBtnDiv className="snsBtnGroup">
+              <a
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <BtnGithub />
+              </a>
+              <a href={project.figma} target="_blank" rel="noopener noreferrer">
+                <BtnFigma />
+              </a>
+              <a
+                href={project.notion}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <BtnFigma />
+              </a>
+            </ProjectBtnDiv>
+          </div>
+          <div className="project-info-detail">
+            <p className="project-date">{project.date}</p>
+            <p>{project.info}</p>
+            <ul>
+              {project.infoDetail.map(el => {
+                return <li>{el}</li>;
+              })}
+            </ul>
+            <div className="tag-group">
+              <Tag key={project.skill.indexOf} items={project.skill} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
+
+const MainImg = styled.div`
+  /* background: center / cover no-repeat
+    url(${process.env.PUBLIC_URL}/assets/${props => props.$image}); */
+`;
+
+// const ProjectTitle = styled.div``;
+
+const ProjectName = styled.div``;
+
+const ProjectBtnDiv = styled.div`
+  margin-top: 4rem;
+  @media ${({ theme }) => theme.tablet} {
+    margin-top: 8rem;
+  }
+`;
